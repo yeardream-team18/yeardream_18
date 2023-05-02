4dd5cfa05723658a5c0b0aa99dd8fc9db43f47d3
a31 <a31@a31s-iMac.local>
2023-05-02 15:12:10 +0900

    fix: Put value type of variable

--- Calculate_BullsCows.py
+++ Calculate_BullsCows.py
@@ -1,5 +1,5 @@
 # Calculates the number of bulls and cows for a guess.
 def get_bulls_cows(guess, secret): # type(int) : guess, secret
-    bulls = sum(guess[i] == secret[i] for i in range(4))
-    cows = sum(guess.count(digit) and guess.index(digit) != secret.index(digit) for digit in secret)    
+    bulls = sum(guess[i] == secret[i] for i in range(4)) # type(bulls) <class 'int'>
+    cows = sum(guess.count(digit) and guess.index(digit) != secret.index(digit) for digit in secret) # type(cows) <class 'int'>   
     return bulls, cows
